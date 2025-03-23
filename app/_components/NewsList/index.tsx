d40c105f75575a8d3097046239294e3cd21359be
Keiko Iwata <oumelab@pt.rdy.jp>
2025-03-23 17:19:19 +0900

    5章まで完了

--- app/_components/NewsList/index.tsx
+++ app/_components/NewsList/index.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 import styles from "./index.module.css";
 import Category from "@/app/_components/Category";
 import Date from "@/app/_components/Date";
-import { News } from "@/app/_libs/microcms";
+import {News} from "@/app/_libs/microcms";
 
 type Props = {
   news: News[];
@@ -20,18 +20,28 @@ export default function NewsList({news}: Props) {
       {news.map((article) => (
         <li key={article.id} className={styles.list}>
           <Link href={`/news/${article.id}`} className={styles.link}>
-            <Image
-              className={styles.image}
-              src="/no-image.png"
-              alt="No Image"
-              width={1200}
-              height={630}
-            />
+            {article.thumbnail ? (
+              <Image
+                src={article.thumbnail.url}
+                alt=""
+                className={styles.image}
+                width={article.thumbnail.width}
+                height={article.thumbnail.height}
+              />
+            ) : (
+              <Image
+                className={styles.image}
+                src="/no-image.png"
+                alt="No Image"
+                width={1200}
+                height={630}
+              />
+            )}
             <dl className={styles.content}>
               <dt className={styles.title}>{article.title}</dt>
               <dd className={styles.meta}>
-               <Category category={article.category} />
-               <Date date={article.publishedAt ?? article.createdAt} />
+                <Category category={article.category} />
+                <Date date={article.publishedAt ?? article.createdAt} />
               </dd>
             </dl>
           </Link>
