d0af04588d0e083bf47d0d194f192bc05562048c
Keiko Iwata <oumelab@pt.rdy.jp>
2025-03-23 21:13:15 +0900

    6章まで完了

--- app/_components/Date/index.tsx
+++ app/_components/Date/index.tsx
@@ -1,20 +1,16 @@
 import Image from "next/image";
 import styles from "./index.module.css";
+import {formDate} from "@/app/_libs/utils";
 
 type Props = {
   date: string;
 };
 
-export default function Date({ date }: Props) {
+export default function Date({date}: Props) {
   return (
     <span className={styles.date}>
-      <Image
-      src="/clock.svg"
-      alt=""
-      width={16}
-      height={16}
-      loading="eager" />
-      {date}
+      <Image src="/clock.svg" alt="" width={16} height={16} loading="eager" />
+      {formDate(date)}
     </span>
-  )
+  );
 }
